c49d46fe7d8f7ff5b14e674987fd4d8b815837d6
dong.phamngoc <cpud1258@gmail.com>
2022-01-16 02:24:19 +0700

    fix master

--- src/config/config.go
+++ src/config/config.go
@@ -30,15 +30,14 @@ var (
 	IsMaster = os.Getenv("MASTER") == "1"
 
 	// Verbose verbose
-	Verbose = ternary(os.Getenv("VERBOSE"), "1") == "1"
+	Verbose     = ternary(os.Getenv("VERBOSE"), "1") == "1"
 	ServiceName = ternary(os.Getenv("SERVICE_NAME"), "API pham ngoc dong")
 
 	// HTTPPort - http port to run
 	HTTPPort, _  = strconv.Atoi(ternary(os.Getenv("HTTP_PORT"), "8088"))
-	HTTPSPort, _  = strconv.Atoi(ternary(os.Getenv("HTTP_PORT"), "8443"))
+	HTTPSPort, _ = strconv.Atoi(ternary(os.Getenv("HTTP_PORT"), "8443"))
 
 	DBConnection = ternary(os.Getenv("PERSONAL_DB_CONNECTION"), "postgresql://postgres:9406715@localhost:5432/personalDB?sslmode=disable")
 	RedisAddr    = ternary(os.Getenv("REDIS_ADDR"), "127.0.0.1:6379")
-	SSLKeyBase64 =os.Getenv("SSL_KEY_64")
-
+	SSLKeyBase64 = os.Getenv("SSL_KEY_64")
 )
