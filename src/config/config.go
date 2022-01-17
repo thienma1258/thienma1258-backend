56ef705ad9866c2f2e70f77f4c6a677a0fe96225
dong.phamngoc <cpud1258@gmail.com>
2022-01-18 00:34:13 +0700

    fix config

--- src/config/config.go
+++ src/config/config.go
@@ -38,7 +38,7 @@ var (
 	HTTPSPort, _ = strconv.Atoi(ternary(os.Getenv("HTTPS_PORT"), "8443"))
 
 	DBConnection = ternary(os.Getenv("PERSONAL_DB_CONNECTION"), "postgresql://postgres:9406715@localhost:5432/personalDB?sslmode=disable")
-	RedisAddr    = ternary(os.Getenv("REDIS_ADDR"), "127.0.0.1:6379")
+	RedisAddr    = ternary(os.Getenv("REDIS_SERVICE_ADDR"), "127.0.0.1:6379")
 	SSLKeyBase64 = os.Getenv("SSL_KEY_64")
 
 	BloggerAPIKey = ternary(os.Getenv("BLOGGER_API_KEY"),"")
